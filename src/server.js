4d38c423d92940d46c9d0eeecdf4cdc377407481
Keanu <keanuryansyah1204@gmail.com>
2023-06-30 12:38:31 +0700

    petshop-react-v1

--- src/server.js
+++ src/server.js
@@ -18,9 +18,9 @@ const conn = mysql.createConnection({
 });
 
 app.use((req, res, next) => {
-  res.setHeader("Acces-Control-Allow-Origin", "http://localhost:5173");
-  res.setHeader("Acces-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Acces-Control-Allow-Headers", "Content-Type,Authorization");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type,Authorization");
   next();
 });
 
